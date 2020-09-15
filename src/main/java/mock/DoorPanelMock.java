ee5bd1fd18e283def8269f39dfb579b9be0eda0c
crystalfn <969274162@qq.com>
2020-09-15 22:01:58 +0800

    [fangni] feat: implement SecurityCenterTest with mockito

--- src/main/java/mock/DoorPanelMock.java
+++ src/main/java/mock/DoorPanelMock.java
@@ -3,7 +3,7 @@ package mock;
 import java.util.stream.Stream;
 
 public class DoorPanelMock extends DoorPanel {
-    void close() {
+    public void close() {
         Stream.of("East Door", "West Door", "North Door", "South Door")
             .forEach((door) -> {
                 System.out.println(door + "is closed");
---
+++ src/test/java/mockito/SecurityCenterTest.java
@@ -0,0 +1,27 @@
+package mockito;
+
+import mock.DoorPanel;
+import mock.SecurityCenter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+@ExtendWith(MockitoExtension.class)
+public class SecurityCenterTest {
+
+    @Mock
+    private DoorPanel mockDoorPanel;
+    @InjectMocks
+    private SecurityCenter securityCenter;
+
+    @Test
+    public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+        verify(mockDoorPanel, times(1)).close();
+    }
+}
