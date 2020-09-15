3a9a162c1de839371a48c7270e7df941071b816b
crystalfn <969274162@qq.com>
2020-09-15 22:14:55 +0800

    [fangni] feat: implement GradeServiceTest with mockito

---
+++ src/test/java/mockito/GradeServiceTest.java
@@ -0,0 +1,32 @@
+package mockito;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import stub.GradeService;
+import stub.GradeSystem;
+import stub.GradeSystemStub;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class GradeServiceTest {
+    @Mock
+    private GradeSystem gradeSystemStub;
+
+    @InjectMocks
+    private GradeService gradeService;
+
+    @Test
+    public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
+        when(gradeSystemStub.gradesFor(1))
+            .thenReturn(Arrays.asList(80.0, 90.0, 100.0));
+        double result = gradeService.calculateAverageGrades(1);
+        Assertions.assertEquals(90.0, result);
+    }
+}
