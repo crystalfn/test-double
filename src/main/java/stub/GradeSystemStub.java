af32a0ae99ed1dd11675607478927e61428f04f1
crystalfn <969274162@qq.com>
2020-09-15 22:06:55 +0800

    [fangni] feat: implement GradeServiceTest with GradeSystemStubb

---
+++ src/main/java/stub/GradeSystemStub.java
@@ -0,0 +1,10 @@
+package stub;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GradeSystemStub extends GradeSystem {
+    public List<Double> gradesFor(long studentId) {
+        return Arrays.asList(80.0, 90.0, 100.0); // 只是一个例子
+    }
+}
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -11,6 +11,9 @@ public class GradeServiceTest {
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-//        Assertions.assertEquals(90.0, result);
+        GradeSystemStub gradeSystem = new GradeSystemStub();
+        GradeService gradeService = new GradeService(gradeSystem);
+        double result = gradeService.calculateAverageGrades(1);
+        Assertions.assertEquals(90.0, result);
     }
 }
